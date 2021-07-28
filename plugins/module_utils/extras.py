97f775991e80031c83e280193c3eea5f87c39a6c
Mikhail Yohman <mikhail.yohman@gmail.com>
2021-07-28 14:59:51 -0500

    Remove __init__ from module utils for each app.

--- plugins/module_utils/extras.py
+++ plugins/module_utils/extras.py
@@ -16,9 +16,6 @@ NB_STATUS = "statuses"
 
 
 class NautobotExtrasModule(NautobotModule):
-    def __init__(self, module, endpoint, client=None, remove_keys=None):
-        super().__init__(module, endpoint, client, remove_keys)
-
     def run(self):
         """
         This function should have all necessary code for endpoints within the application
