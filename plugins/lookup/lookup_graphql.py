233a08b6ba956a93dcb7d1d10963fe48ad8b9e37
Josh VanDeraa <jv@networktocode.com>
2021-09-10 15:20:58 -0500

    Fixes shebang issue

--- plugins/lookup/lookup_graphql.py
+++ plugins/lookup/lookup_graphql.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 # Copyright: (c) 2020, Network to Code (@networktocode) <info@networktocode.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
