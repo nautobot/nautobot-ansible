45796927c10a1a99556aa740650dd4354773e12e
Ken Celenza <ken@celenza.org>
2021-09-16 11:31:40 -0400

    Update plugins/action/query_graphql.py
    Co-authored-by: Josh VanDeraa <jv@networktocode.com>

--- plugins/action/query_graphql.py
+++ plugins/action/query_graphql.py
@@ -46,7 +46,7 @@ def nautobot_action_graphql(args):
         raise AnsibleError("validate_certs must be a boolean")
 
     update_hostvars = args.get("update_hostvars", False)
-    Display().vv("Populate root is set to: %s" % update_hostvars)
+    Display().vv("Update hostvars is set to: %s" % update_hostvars)
 
     # Verify SSL Verify is of boolean
     if not isinstance(update_hostvars, bool):
