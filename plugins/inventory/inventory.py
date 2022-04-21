41659f4e13a293c02082bd56a5d652b5dbcca967
Mikhail Yohman <mikhail.yohman@gmail.com>
2022-04-20 21:13:30 -0600

    Fix linting issue

--- plugins/inventory/inventory.py
+++ plugins/inventory/inventory.py
@@ -1071,7 +1071,7 @@ class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
         openapi = self._fetch_information(self.api_endpoint + "/api/docs/?format=openapi")
 
         self.api_version = openapi["info"]["version"]
-        
+
         device_path = "/api/dcim/devices/" if "/api/dcim/devices/" in openapi["paths"] else "/dcim/devices/"
         vm_path = "/api/virtualization/virtual-machines/" if "/api/virtualization/virtual-machines/" in openapi["paths"] else "/virtualization/virtual-machines"
 
