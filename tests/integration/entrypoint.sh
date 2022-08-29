3e216c4e283d62a70c9022f6d2ca101b6b26fe1e
Mikhail Yohman <mikhail.yohman@gmail.com>
2022-08-28 19:16:41 -0600

    Syntax error.

--- tests/integration/entrypoint.sh
+++ tests/integration/entrypoint.sh
@@ -24,16 +24,15 @@ function main {
     echo "# Rendering integration configuration"
     render "./tests/integration/integration_config.tmpl.yml" > ./tests/integration/integration_config.yml
 
-    # echo "# Checking to make sure Nautobot server is reachable.."
-    # timeout 300 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' nautobot:8000)" != "200" ]]; do echo "waiting for Nautobot"; sleep 5; done' || false
+    echo "# Checking to make sure Nautobot server is reachable.."
+    timeout 300 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' nautobot:8000)" != "200" ]]; do echo "waiting for Nautobot"; sleep 5; done' || false
 
-    # echo "# Populating Nautobot for running integration tests.."
-    # python ./tests/integration/nautobot-populate.py
+    echo "# Populating Nautobot for running integration tests.."
+    python ./tests/integration/nautobot-populate.py
 
     echo "# Running..."
     # shellcheck disable=SC2086
-    if [[ "${INVOKE_NAUTOBOT_ANSIBLE_NAUTOBOT_VER:-}" == "1.4"]]
-    then
+    if [[ "${INVOKE_NAUTOBOT_ANSIBLE_NAUTOBOT_VER:-}" == "1.4" ]]; then
       ansible-test integration $ANSIBLE_INTEGRATION_ARGS --coverage --python "$PYTHON_VERSION" inventory-1.4 "$@"
     else
       ansible-test integration $ANSIBLE_INTEGRATION_ARGS --coverage --python "$PYTHON_VERSION" inventory "$@"
