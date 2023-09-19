906ff263906019ce73e9b3edd8add2c6a92486af
pszulczewski <patryk@szulczewski.eu>
2023-09-19 13:50:51 +0200

    Bump up to rc.3

--- tasks.py
+++ tasks.py
@@ -24,7 +24,7 @@ namespace = Collection("nautobot_ansible")
 namespace.configure(
     {
         "nautobot_ansible": {
-            "nautobot_ver": "2.0.0-rc.2",
+            "nautobot_ver": "2.0.0-rc.3",
             "project_name": "nautobot_ansible",
             "python_ver": "3.9",
             "local": False,
