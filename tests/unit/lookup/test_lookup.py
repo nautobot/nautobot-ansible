1181d1fc1e2451df5434b5c44023fd9354f8ccc8
Christian Adell <chadell@gmail.com>
2024-12-31 15:45:07 +0100

    filters test

--- tests/unit/lookup/test_lookup.py
+++ tests/unit/lookup/test_lookup.py
@@ -1,6 +1,10 @@
 """Tests for Nautobot Query Lookup Plugin."""
 
-from ansible.errors import AnsibleError, AnsibleLookupError
+from ansible.errors import AnsibleError
+from ansible.template import Templar
+from ansible.parsing.dataloader import DataLoader
+from ansible.vars.manager import VariableManager
+from ansible.inventory.manager import InventoryManager
 import pytest
 from unittest.mock import patch, MagicMock
 
@@ -56,3 +60,56 @@ def test_no_token_or_endpoint(mock_pynautobot, lookup):
     """Test when neither token nor endpoint is provided."""
     with pytest.raises(AnsibleError):
         lookup.run(["devices"], token=None, api_endpoint=None)
+
+
+@patch("plugins.lookup.lookup.pynautobot.api")
+def test_run_with_static_filter(mock_pynautobot, lookup):
+    """Test filters functionality of the run method."""
+    mock_api = MagicMock()
+    mock_pynautobot.return_value = mock_api
+    mock_api.dcim.devices.filter.return_value = [{"id": 1, "name": "device1"}]
+
+    # Initialize Ansible's Templar with necessary components
+    loader = DataLoader()
+    inventory = InventoryManager(loader=loader, sources=[])
+    variable_manager = VariableManager(loader=loader, inventory=inventory)
+    templar = Templar(loader=loader, variables=variable_manager.get_vars())
+    lookup._templar = templar
+
+    terms = ["devices"]
+    kwargs = {
+        "token": "fake-token",
+        "api_endpoint": "https://nautobot.local",
+        "api_filter": "{'name': 'device1'}",
+    }
+    result = lookup.run(terms, **kwargs)
+
+    mock_pynautobot.assert_called_once_with("https://nautobot.local", token="fake-token", api_version=None, verify=True, retries="0")
+    mock_api.dcim.devices.filter.assert_called_once_with(_raw_params=["{'name':", "'device1'}"])
+    assert result == [{"key": 1, "value": {"id": 1, "name": "device1"}}], "Expected a successful result"
+
+
+@patch("plugins.lookup.lookup.pynautobot.api")
+def test_run_with_dynamic_filter(mock_pynautobot, lookup):
+    """Test dynamic filters functionality of the run method."""
+    mock_api = MagicMock()
+    mock_pynautobot.return_value = mock_api
+    mock_api.dcim.devices.filter.return_value = [{"id": 1, "name": "device1"}]
+
+    # Initialize Ansible's Templar with necessary components
+    loader = DataLoader()
+    variables = {"device_name": "device1"}
+    templar = Templar(loader=loader, variables=variables)
+    lookup._templar = templar
+
+    terms = ["devices"]
+    kwargs = {
+        "token": "fake-token",
+        "api_endpoint": "https://nautobot.local",
+        "api_filter": "{'name': '{{ device_name }}'}",
+    }
+    result = lookup.run(terms, **kwargs)
+
+    mock_pynautobot.assert_called_once_with("https://nautobot.local", token="fake-token", api_version=None, verify=True, retries="0")
+    mock_api.dcim.devices.filter.assert_called_once_with(_raw_params=["{'name':", "'device1'}"])
+    assert result == [{"key": 1, "value": {"id": 1, "name": "device1"}}], "Expected a successful result"
