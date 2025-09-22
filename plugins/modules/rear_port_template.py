d84b7d575219a0d4d5f64534522215625c7472d0
Joe Wesch <joe.wesch@networktocode.com>
2025-09-22 17:45:06 -0500

    Adds missing custom_fields and tags support

--- plugins/modules/rear_port_template.py
+++ plugins/modules/rear_port_template.py
@@ -23,6 +23,7 @@ version_added: "1.0.0"
 extends_documentation_fragment:
   - networktocode.nautobot.fragments.base
   - networktocode.nautobot.fragments.id
+  - networktocode.nautobot.fragments.custom_fields
 options:
   device_type:
     description:
@@ -122,6 +123,7 @@ from ansible_collections.networktocode.nautobot.plugins.module_utils.dcim import
     NautobotDcimModule,
 )
 from ansible_collections.networktocode.nautobot.plugins.module_utils.utils import (
+    CUSTOM_FIELDS_ARG_SPEC,
     ID_ARG_SPEC,
     NAUTOBOT_ARG_SPEC,
 )
@@ -133,6 +135,7 @@ def main():
     """
     argument_spec = deepcopy(NAUTOBOT_ARG_SPEC)
     argument_spec.update(deepcopy(ID_ARG_SPEC))
+    argument_spec.update(deepcopy(CUSTOM_FIELDS_ARG_SPEC))
     argument_spec.update(
         dict(
             device_type=dict(required=False, type="raw"),
